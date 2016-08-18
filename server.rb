90f100c0748acea40884e9870043878e1f2439b9
John Bell <Johnrbell@gmail.com>
2016-08-18 18:19:23 -0400

    refactor

--- server.rb
+++ server.rb
@@ -9,9 +9,6 @@ class Application<Sinatra::Base
 
   get '/api/v1/trains' do
     train_data = get_train_data(params['long'])
-    if params['long'] != "true" #deletes the detailed info if you didnt ask for it. 
-      train_data.each { |h| h.delete(:detail) } 
-    end
     train_data.to_json #response
   end
 
@@ -19,13 +16,13 @@ class Application<Sinatra::Base
     train_data = get_train_data(params['long'])
     train_to_get = params['id'].upcase
     train_data = train_data.find { |x| x[:train] == train_to_get } #looks for single, deletes the rest
-    if params['long'] != "true" #deletes the detailed info if you didnt ask for it. 
-      train_data.delete(:detail)
-    end
+    # if params['long'] != "true" #deletes the detailed info if you didnt ask for it. 
+    #   train_data.delete(:detail)
+    # end
     train_data.to_json #response
   end
 
-  def get_train_data(long) #this gets the train data and puts it into a simple, usable hash. 
+  def get_train_data(request_long) #this gets the train data and puts it into a simple, usable hash. 
     group_count = 10 #this is how many train groups the xml file has. i.e. ABC is one group.
     
     data = Nokogiri::XML(open('http://web.mta.info/status/serviceStatus.txt')) #opens god-awful xml file.
@@ -35,15 +32,15 @@ class Application<Sinatra::Base
     status.map! do |train| train = train.text.to_s end #reduces nokogiri object to simple array. 
     long_status = data.xpath('//subway').xpath('//text').first(group_count) #get X status for trains from xml
     if long_status != nil #this is a check to see if the xml file had long stats
-      long_status.map! do |train| train = train.text.to_s end #reduces nokogiri object to simple array. 
+        cleanUpDetail(long_status)
     end
 
-    buildResponse(trains,status,long_status) #build the response
+    buildResponse(trains,status,long_status,request_long) #build the response
     
     @final_hash #return hash 
   end
 
-  def buildResponse(trains,status,long_status) #builds the reponse object. needs to be DRYer
+  def buildResponse(trains,status,long_status,request_long) #builds the reponse object. needs to be DRYer
     @final_hash = [] #blank hash for final ouput
     work_hashing = trains.zip(status,long_status) #zips the arrays together.
     work_hashing.each do |train,status,long_status| #split multiple trains to individual. ACE > A,C,E
@@ -52,5 +49,17 @@ class Application<Sinatra::Base
         @final_hash.push(single_train) #after split, push to new array. 
       end
     end
+    if request_long != "true" #deletes the detailed info if you didnt ask for it. 
+      @final_hash.each { |h| h.delete(:detail) } 
+    end
+  end
+
+  def cleanUpDetail(long_status) #start of cleaning up detail response (fuuuuuuuck)
+    long_status.map! do |train| 
+        train = train.text.to_s #reduces nokogiri object to simple array. 
+        train = train.gsub(/\n+ */, "").gsub("&nbsp;","").gsub('<br/>','')
+        p train    
+      end 
+    
   end
 end
